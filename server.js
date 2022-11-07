37b4c0acd13267ae38de25c6b9d2da6178e2460c
MHN715 <nielsen.177@gmail.com>
2022-11-07 17:47:21 +0100

    switch fron multer to express-fileupload

--- server.js
+++ server.js
@@ -1,49 +1,69 @@
-require("dotenv").config()
-const multer = require("multer")
+require("dotenv").config();
 const path = require("path");
+const fileUpload = require("express-fileupload"); // Express framework middleware
+const express = require("express");
 
-const express = require("express")
-const app = express()
-app.use(express.urlencoded({ extended: true }))
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    createParentPath: true, // enabling "express-fileupload middleware". createParentPath will let express-fileupload create the dir path for "mv" method when said dir doesn't exist
+  })
+);
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
-
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      file.fieldname + '-' + Date.now() + path.extname(file.originalname),
-    )
-  },
-})
-
-const upload = multer({ storage: storage })
-
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index")
-})
+  res.render("index");
+});
 
+app.use(express.static(__dirname + "/public"));
 
-app.post("/upload", upload.single("file"), async (req, res) => {
-  
-  res.render("index", { fileLink: `${req.headers.origin}/file/${req.file.filename}` })
-})
+let currentRandomFolderName;
 
-app.route("/file/:id").get(handleDownload).post(handleDownload)
+function randomizeString(characters) {
+  let result = "";
+  charactersLength = characters.length;
 
-async function handleDownload(req, res) {
-
-  // console.log(req.params.id)
-  // console.log(req)
-  // console.log("test")
-  // console.log(extension)
-
-  res.download(__dirname + `/uploads/${req.params.id}`, req.params.id)
-
+  for (var i = 0; i < charactersLength; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  currentRandomFolderName = result;
+  return result;
 }
 
-app.listen(process.env.PORT)
+app.post("/uploads", (req, res) => {
+  if (!req.files) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  const file = req.files.file;
+  console.log("File uploaded:", file);
+
+  const uploaded_files_folder =
+    __dirname +
+    "/uploads/" +
+    `/${randomizeString("abcdefg1234567")}/` +
+    file.name;
+
+  file.mv(uploaded_files_folder, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    return res.render("index", {
+      fileLink: `${req.headers.origin}/uploads/${currentRandomFolderName}/${file.name}`,
+    });
+  });
+});
+
+app.route("/uploads/:folder/:file").get(handleDownload).post(handleDownload);
+
+async function handleDownload(req, res) {
+  console.log(
+    "File requested:",
+    "/" + req.params.folder + "/" + req.params.file
+  );
+  res.download(__dirname + `/uploads/${req.params.folder}/${req.params.file}`);
+}
+
+app.listen(process.env.PORT);
