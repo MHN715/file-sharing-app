3e923c6767d2d984f86c819101a9d7dda3abeb0a
MHN715 <nielsen.177@gmail.com>
2022-11-11 14:05:37 +0100

    copy btn

--- public/script.js
+++ public/script.js
@@ -1,43 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const copyBtn = document.getElementById("copy-text-btn");
+  const textToCopy = document.querySelector(".generated-atag");
+  copyToClipBoard(copyBtn, textToCopy);
   submitWhenFileSelected();
-
-  const testClass = document.querySelector(".testClass");
-  const linksResults = document.querySelector(".links-results");
-
-  //   console.log(testClass?.textContent);
-
-  let testObject = {};
-
-  let testClassText = testClass?.textContent;
-
-  console.log(testClassText);
-
-  testObject.link = testClassText;
-
-  console.log(testObject);
-
-  //   const form = document.querySelector("form"),
-  //     fileInput = document.querySelector(".file-input"),
-  //     progressArea = document.querySelector(".progress-area"),
-  //     uploadedArea = document.querySelector(".uploaded-area");
-
-  //   form.addEventListener("click", (e) => {
-  //     console.log(e);
-  //     fileInput.click();
-  //   });
-
-  //   fileInput.onchange = ({ target }) => {
-  //     let file = target.files[0];
-  //     if (file) {
-  //       let fileName = file.name;
-  //       console.log(file);
-  //       console.log(fileName);
-  //     }
-  //   };
 });
 
 function submitWhenFileSelected() {
   document.getElementById("form").onchange = function () {
     document.getElementById("form").submit();
   };
+}
+
+function copyToClipBoard(copyBtn, textToCopy) {
+  if (!textToCopy) return;
+  copyBtn.addEventListener("click", async (event) => {
+    if (!navigator.clipboard) {
+      // Clipboard API not available
+      return;
+    }
+    const text = textToCopy?.textContent;
+    try {
+      await navigator.clipboard.writeText(text);
+      event.target.textContent = "Copied to clipboard";
+    } catch (err) {
+      console.error("Failed to copy!", err);
+    }
+  });
 }
